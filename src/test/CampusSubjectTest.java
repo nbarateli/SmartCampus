cb8bd469f1935416f68ba742a558825f9a7218c1
ninah <nkhad15@freeuni.edu.ge>
2017-06-17 20:56:22 +0400

    added unit tests for CampusSubject

---
+++ src/test/CampusSubjectTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.Date;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.accounts.UserProblem;
+import model.lecture.CampusSubject;
+
+import static org.junit.Assert.*;
+
+public class CampusSubjectTest {
+    
+    private CampusSubject subject;
+    
+    @Before 
+    public void createUser() {
+        subject = new CampusSubject(1, "კალკულუსი");
+    }
+    
+    @Test
+    public void test() {
+        assertEquals(1, subject.getID());
+        assertEquals("კალკულუსი", subject.getName());
+    }
+
+}
