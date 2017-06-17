ec231cbc1916040e2eebfc35ccb76d205e4671a3
nbarateli <nbara15@freeuni.edu.ge>
2017-06-17 15:08:34 +0400

    Merge branch 'master' of D:\FREEUNI\IV Sem\Software Engineering\Final Project\SmartCampus with conflicts.

--- src/serve/rooms/RoomAdder.java
+++ src/serve/rooms/RoomAdder.java
@@ -1,28 +1,19 @@
 package serve.rooms;
 
-import misc.Utils;
-import model.rooms.Room;
-import model.rooms.Room.RoomType;
-import model.rooms.Room.SeatType;
-import model.rooms.RoomSearchQuery;
-import model.rooms.manager.RoomManager;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import static misc.WebConstants.ROOM_MANAGER;
 
 /**
  * Servlet implementation class RoomAdder
  */
-@WebServlet(name = "Room Adder", urlPatterns = {"/rooms/addroom"})
+@WebServlet(name = "RoomAdder", urlPatterns = "/rooms/addroom")
 public class RoomAdder extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -30,67 +21,21 @@ public class RoomAdder extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-    
-    /*
-     * checks if given string is a positive number
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    private boolean numberStringIsValid(String s) {
-        try {
-            int numValue = Integer.parseInt(s);
-            //capacity and floor can't be negative
-            //their input types are "number" but some old browsers don't have support for
-            //that and we don't won't any exceptions
-            if(numValue <= 0)
-                return false;
-        } catch (NumberFormatException e) {
-            return false; //the text wasn't a number
-        }
-        
-        return true;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
     }
-    
-    /*
-     * checks if room with given name already exists
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    private boolean roomExists(String name, RoomManager manager) {
-        RoomSearchQuery query = new RoomSearchQuery();
-        query.setName(name);
-        return !manager.find(query).isEmpty();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
     }
-    
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    RoomManager manager = 
-                (RoomManager) request.getServletContext().getAttribute(ROOM_MANAGER);
-        String name = request.getParameter("room_name");
-        String floorStr = request.getParameter("room_floor");
-        String capacityStr = request.getParameter("capacity");
-        RoomType roomType = Utils.toRoomType(request.getParameter("room_type"));
-        SeatType seatType = Utils.toSeatType(request.getParameter("seat_type").toUpperCase());
-        boolean available = (request.getParameter("can_be_booked") != null);
-        
-        //add to database only if every field has valid input
-        if (!name.equals("") && numberStringIsValid(floorStr) 
-                && numberStringIsValid(capacityStr) && !roomExists(name, manager)) {
-            int floor = Integer.parseInt(floorStr);
-            int capacity = Integer.parseInt(capacityStr);
-
-            Room room = new Room(-1, capacity, name, roomType, seatType, 
-                    available, floor);
-            manager.add(room);
-        }
-
-        response.sendRedirect("/data/addingData.jsp");
-    }
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
