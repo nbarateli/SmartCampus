eb05ac8fed518be6183e48401156010c8cb3e2b5
nbarateli <nbara15@freeuni.edu.ge>
2017-06-23 16:38:42 +0400

    Implemented LectureRemover

--- src/serve/lecture/LectureRemover.java
+++ src/serve/lecture/LectureRemover.java
@@ -1,11 +1,16 @@
 package serve.lecture;
 
+import model.lectures.manager.LectureManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import static misc.WebConstants.*;
 
 /**
  * Servlet implementation class LectureRemover
@@ -23,19 +28,29 @@ public class LectureRemover extends HttpServlet {
     }
 
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
-
-    /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
+        response.setCharacterEncoding("UTF-8");
+        LectureManager manager = (LectureManager) request.getServletContext().getAttribute(LECTURE_MANAGER);
+        PrintWriter out = response.getWriter();
+        if ("true".equals(request.getParameter("remove_all"))) {
+            manager.removeAllLectures();
+            out.println(SUCCESS + ": all lectures removed.");
+            return;
+        }
+
+        try {
+            int id = Integer.parseInt(request.getParameter("lecture_id"));
+            if (manager.remove(id)) {
+                out.println(SUCCESS + ": lecture removed");
+            } else {
+                out.println(FAILED + ": lecture not found");
+            }
+
+        } catch (NumberFormatException e) {
+            out.println(FAILED + ": bad parameter");
+        }
     }
 
 }
