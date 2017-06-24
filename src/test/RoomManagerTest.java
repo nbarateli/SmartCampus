67c1b36bca2c75c689704e6b95dfbf386df3e13b
desmart18 <dmagh15@freeuni.edu.ge>
2017-06-24 23:47:52 +0400

    tests
    New tests added for RoomManager class

--- src/test/RoomManagerTest.java
+++ src/test/RoomManagerTest.java
@@ -25,6 +25,7 @@ import static org.junit.Assert.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +36,11 @@ public class RoomManagerTest {
     public void setUp() {
         manager = (DefaultRoomManager) DefaultRoomManager.getInstance();
     }
+	
+	/*
+	 * Test1
+	 * Tests methods find and add of RoomManagerClass
+	 */
 	@Test
 	public void test1() {
 		RoomSearchQuery query = new RoomSearchQuery();
@@ -55,9 +61,12 @@ public class RoomManagerTest {
 		list = manager.find(query);
 		y = list.size();
 		assertEquals(x + 3, y);
-		
 	}
 	
+	/*
+	 * Test2
+	 * Tests methods find, remove and getRoomByName of RoomManagerClass
+	 */
 	@Test
 	public void test2() {
 		RoomSearchQuery query = new RoomSearchQuery();
@@ -77,31 +86,35 @@ public class RoomManagerTest {
 		assertEquals(x - 3, y);
 	}
 	
+	/*
+	 * Test3
+	 * Tests methods find, addImage and getAllImagesOf of RoomManagerClass
+	 */
 	@Test
 	public void test3() {
 		RoomSearchQuery query = new RoomSearchQuery();
 		List<Room> list =  manager.find(query);
 		Room room1 = list.get(0);
-		//System.out.println(room1.getID() + " " + room1.getRoomType());
 		List<String> listOfImages =  manager.getAllImagesOf(room1);
 		int x = listOfImages.size();
 		manager.addImage(room1, "test24.jpg");
 		listOfImages =  manager.getAllImagesOf(room1);
 		int y = listOfImages.size();
-		//System.out.println(x+1 + " " + y);
 		assertEquals(x+1, y);
 		manager.addImage(room1, "test2");
 		listOfImages =  manager.getAllImagesOf(room1);
 		y = listOfImages.size();
-		//System.out.println(x+2 + " " + y);
 		assertEquals(x+2, y);
 		manager.addImage(room1, "test3");
 		listOfImages =  manager.getAllImagesOf(room1);
 		y = listOfImages.size();
-		//System.out.println(x+3 + " " + y);
 		assertEquals(x+3, y);
 	}
 	
+	/*
+	 * Test4
+	 * Tests methods find, findAllLecturesAt and addAll of RoomManagerClass
+	 */
 	@Test
 	public void test4() {
 		RoomSearchQuery query = new RoomSearchQuery();
@@ -117,5 +130,32 @@ public class RoomManagerTest {
 		lecturesList2.addAll(manager.findAllLecturesAt(room1, WeekDay.SUNDAY));
 		assertTrue(lecturesList.equals(lecturesList2));
 	}
-
+	
+	/*
+	 * Test5
+	 * Tests methods findAllLecturesAt and getRoomById of RoomManagerClass
+	 */
+	@Test
+	public void test5() {
+		Room room1 = manager.getRoomById(4);
+		List<Lecture> lecturesList = manager.findAllLecturesAt(room1);
+		Lecture lec = lecturesList.get(0);
+		List<Lecture> lecturesList2 = manager.findAllLecturesAt(room1, lec.getDay(), lec.getStartTime(), lec.getEndTime());
+		assertEquals(lecturesList2.size(), 1);
+		assertTrue(lecturesList2.get(0).equals(lec));
+	}
+	
+	/*
+	 * Test6
+	 * Tests methods findAllLecturesAt and getRoomByName of RoomManagerClass
+	 */
+	@Test
+	public void test6() {
+		Room room1 = manager.getRoomByName("408");
+		List<Lecture> lecturesList = manager.findAllLecturesAt(room1);
+		Lecture lec = lecturesList.get(0);
+		List<Lecture> lecturesList2 = manager.findAllLecturesAt(room1, lec.getDay(), lec.getStartTime(), lec.getEndTime());
+		assertEquals(lecturesList2.size(), 1);
+		assertTrue(lecturesList2.get(0).equals(lec));
+	}
 }
