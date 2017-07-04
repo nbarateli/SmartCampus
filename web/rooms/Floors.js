11fc592d00c2f1afaa8ab8ad5d3b9be10459b454
nbarateli <nbara15@freeuni.edu.ge>
2017-07-04 11:12:01 +0400

    fixed display problems in rooms.jsp

--- web/rooms/Floors.js
+++ web/rooms/Floors.js
@@ -4,6 +4,7 @@
 window.addEventListener('load', load, false);
 
 function load() {
+    console.log("lodaded");
     var map1 = document.getElementById("map1");
     var map2 = document.getElementById("map2");
     var map3 = document.getElementById("map3");
@@ -45,128 +46,125 @@ function workMaps(map) {
 
 
 function roomClicked() {
+    //testing
+    var id = this.id;
+    var modal = document.getElementById('myModal');
+    console.log(id);
+    $.get('/rooms/room', {name: id.substring(1)},
+        function (returnedData) {
+            displayRoom(returnedData, 'roomForm');
+        }).fail(function () {
+        console.log("error");
+    });
 
-    function myClick() {
-        //testing
-        var id = this.id;
-        var modal = document.getElementById('myModal');
-        console.log(id);
-        $.get('/rooms/room', {name: id.substring(1)},
-            function (returnedData) {
-                displayRoom(returnedData, 'roomForm');
-            }).fail(function () {
-            console.log("error");
-        });
+    modal.style.display = "block";
+}
 
-        modal.style.display = "block";
+function myHover() {
+    return;
+    var id = this.id;
+    var rect = document.getElementById(id).getBoundingClientRect();
+    var coords = this.getAttribute("coords").split(",");
+    var leftPos = +rect.left + 130 + +coords[2];
+    var topPos = +rect.top + +coords[3];
+
+    console.log("sum is: " + leftPos + " " + topPos);
+    console.log(leftPos + " " + topPos);
+    var div = document.getElementById("popup-div");
+    div.removeChild(div.childNodes[0]);
+    var text = document.createTextNode("this room is: " + id);
+    div.appendChild(text);
+
+    div.style.backgroundColor = "yellow";
+    div.style.border = "1px";
+    div.style.position = "absolute";
+    div.style.left = leftPos + "px";
+    div.style.top = topPos + "px"; //div.style.right = "2px"; div.style.bottom = "2px";
+    div.style.height = "100px";
+    div.style.width = "100px";
+    document.body.appendChild(div);
+
+    document.getElementById("par").innerHTML = id;
+}
+
+function floorChange() {
+    var x = document.getElementById("floors").value;
+    if (x === "first") showFirst();
+    if (x === "second") showSecond();
+    if (x === "third") showThird();
+    if (x === "fourth") showFourth();
+}
+
+function showFirst() {
+    document.getElementById("map2").style.display = "none";
+    document.getElementById("map3").style.display = "none";
+    document.getElementById("map4").style.display = "none";
+    document.getElementById("map1").style.display = "unset";
+}
+
+function showSecond() {
+    document.getElementById("map1").style.display = "none";
+    document.getElementById("map3").style.display = "none";
+    document.getElementById("map4").style.display = "none";
+    document.getElementById("map2").style.display = "unset";
+}
+
+function showThird() {
+    document.getElementById("map1").style.display = "none";
+    document.getElementById("map2").style.display = "none";
+    document.getElementById("map4").style.display = "none";
+    document.getElementById("map3").style.display = "unset";
+}
+
+function showFourth() {
+    document.getElementById("map1").style.display = "none";
+    document.getElementById("map2").style.display = "none";
+    document.getElementById("map3").style.display = "none";
+    document.getElementById("map4").style.display = "unset";
+}
+
+function highlightRooms(room) {
+    var roomName = JSON.stringify(room);
+    var area = document.getElementById(roomName);
+};
+
+function displayRoom(data, id) {
+    var tmp = document.getElementById("mustmpl").innerHTML;
+
+    function parseRoomType(roomtype) {
+        switch (roomtype.toString().toLowerCase()) {
+            case 'laboratory':
+                return "ლაბორატორია";
+            case 'auditorium':
+                return "აუდიტორია"
+        }
+        return "სხვა";
     }
 
-    function myHover() {
-        return;
-        var id = this.id;
-        var rect = document.getElementById(id).getBoundingClientRect();
-        var coords = this.getAttribute("coords").split(",");
-        var leftPos = +rect.left + 130 + +coords[2];
-        var topPos = +rect.top + +coords[3];
-
-        console.log("sum is: " + leftPos + " " + topPos);
-        console.log(leftPos + " " + topPos);
-        var div = document.getElementById("popup-div");
-        div.removeChild(div.childNodes[0]);
-        var text = document.createTextNode("this room is: " + id);
-        div.appendChild(text);
-
-        div.style.backgroundColor = "yellow";
-        div.style.border = "1px";
-        div.style.position = "absolute";
-        div.style.left = leftPos + "px";
-        div.style.top = topPos + "px"; //div.style.right = "2px"; div.style.bottom = "2px";
-        div.style.height = "100px";
-        div.style.width = "100px";
-        document.body.appendChild(div);
-
-        document.getElementById("par").innerHTML = id;
-    }
-
-    function floorChange() {
-        var x = document.getElementById("floors").value;
-        if (x === "first") showFirst();
-        if (x === "second") showSecond();
-        if (x === "third") showThird();
-        if (x === "fourth") showFourth();
-    }
-
-    function showFirst() {
-        document.getElementById("map2").style.display = "none";
-        document.getElementById("map3").style.display = "none";
-        document.getElementById("map4").style.display = "none";
-        document.getElementById("map1").style.display = "unset";
-    }
-
-    function showSecond() {
-        document.getElementById("map1").style.display = "none";
-        document.getElementById("map3").style.display = "none";
-        document.getElementById("map4").style.display = "none";
-        document.getElementById("map2").style.display = "unset";
-    }
-
-    function showThird() {
-        document.getElementById("map1").style.display = "none";
-        document.getElementById("map2").style.display = "none";
-        document.getElementById("map4").style.display = "none";
-        document.getElementById("map3").style.display = "unset";
-    }
-
-    function showFourth() {
-        document.getElementById("map1").style.display = "none";
-        document.getElementById("map2").style.display = "none";
-        document.getElementById("map3").style.display = "none";
-        document.getElementById("map4").style.display = "unset";
-    }
-
-    function highlightRooms(room) {
-        var roomName = JSON.stringify(room);
-        var area = document.getElementById(roomName);
+    var parseSeatType = function (seattype) {
+        switch (seattype.toString().toLowerCase()) {
+            case 'desks':
+                return 'მერხები';
+            case 'chairs':
+                return 'სკამები';
+            case 'computers':
+                return 'კომპიუტერები';
+            case 'tables':
+                return 'მაგიდები';
+        }
     };
+    data['roomtype'] = parseRoomType(data['roomtype']);
 
-    function displayRoom(data, id) {
-        var tmp = document.getElementById("mustmpl").innerHTML;
-
-        function parseRoomType(roomtype) {
-            switch (roomtype.toString().toLowerCase()) {
-                case 'laboratory':
-                    return "ლაბორატორია";
-                case 'auditorium':
-                    return "აუდიტორია"
-            }
-            return "სხვა";
-        }
-
-        var parseSeatType = function (seattype) {
-            switch (seattype.toString().toLowerCase()) {
-                case 'desks':
-                    return 'მერხები';
-                case 'chairs':
-                    return 'სკამები';
-                case 'computers':
-                    return 'კომპიუტერები';
-                case 'tables':
-                    return 'მაგიდები';
-            }
-        };
-        data['roomtype'] = parseRoomType(data['roomtype']);
-
-        data['seattype'] = parseSeatType(data['seattype']);
-        var available = data['available'];
-        if (available) {
-            data['available'] = 'კი';
-            data['spanid'] = 'yes-span';
-        } else {
-            data['available'] = 'არა';
-            data['spanid'] = 'no-span';
-        }
-        html = Mustache.to_html(tmp, data);
-        var box = document.getElementById(id);
-        box.innerHTML = html;
+    data['seattype'] = parseSeatType(data['seattype']);
+    var available = data['available'];
+    if (available) {
+        data['available'] = 'კი';
+        data['spanid'] = 'yes-span';
+    } else {
+        data['available'] = 'არა';
+        data['spanid'] = 'no-span';
     }
+    html = Mustache.to_html(tmp, data);
+    var box = document.getElementById(id);
+    box.innerHTML = html;
 }
