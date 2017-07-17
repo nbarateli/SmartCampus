b43e730bff061dd6ab212caf46abf25a4065b4bf
ninah <nkhad15@freeuni.edu.ge>
2017-07-18 01:22:44 +0400

    added even more comments

--- src/model/accounts/UserSearchQueryGenerator.java
+++ src/model/accounts/UserSearchQueryGenerator.java
@@ -4,10 +4,10 @@ import model.campus.CampusSearchQuery;
 import model.campus.CampusSearchQueryGenerator;
 
 public class UserSearchQueryGenerator implements CampusSearchQueryGenerator<User> {
+
     @Override
     public CampusSearchQuery generateQuery() {
         return null;
     }
-
 
 }
